3911ee78a57a6f72432337ac963aa8ff56a67bd9
Buddhika Senanayake <bnsbuddhika@gmail.com>
2024-06-13 19:25:11 +0530

    Swagger config

--- src/main/java/com/maathru/backend/Application/controllers/v1/LoginController.java
+++ src/main/java/com/maathru/backend/Application/controllers/v1/LoginController.java
@@ -1,4 +1,4 @@
-package com.maathru.backend.Application.controllers;
+package com.maathru.backend.Application.controllers.v1;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/maathru/backend/Application/controllers/v1/UserController.java
+++ src/main/java/com/maathru/backend/Application/controllers/v1/UserController.java
@@ -1,17 +1,14 @@
-package com.maathru.backend.Application.controllers;
+package com.maathru.backend.Application.controllers.v1;
 
 import com.maathru.backend.Application.dto.request.CreateUserDto;
 import com.maathru.backend.Application.dto.response.UserProfileDto;
 import com.maathru.backend.Domain.entity.User;
 import com.maathru.backend.Domain.service.UserService;
-import com.maathru.backend.External.repository.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/user")
