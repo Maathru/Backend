a0fd61109f9ed4febd72151289c07d41be8dffd8
Chamika Madhushan <chamikamadhushan35@gmail.com>
2024-07-16 23:58:29 +0530

    Repos added

--- src/main/java/com/maathru/backend/External/repository/eligible/MedicalHistoryRepository.java
+++ src/main/java/com/maathru/backend/External/repository/eligible/MedicalHistoryRepository.java
@@ -1,4 +1,4 @@
-package com.maathru.backend.External.repository;
+package com.maathru.backend.External.repository.eligible;
 
 import com.maathru.backend.Domain.entity.eligible.MedicalHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
