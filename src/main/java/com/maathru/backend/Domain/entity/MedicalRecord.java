756094e805c86c83eeefe4d076b0c762138f7f75
chamikam <2021is031@ucsc.cmb.ac.lk>
2024-11-26 13:49:50 +0530

    added medical record entity

---
+++ src/main/java/com/maathru/backend/Domain/entity/MedicalRecord.java
@@ -0,0 +1,30 @@
+package com.maathru.backend.Domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "medical_record")
+public class MedicalRecord extends Auditable {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(updatable = false, unique = true, nullable = false)
+  private Long recordId;
+
+  @ManyToOne(cascade = CascadeType.ALL, optional = false)
+  @JoinColumn(name = "patient_id", nullable = false)
+  private User patient;
+
+  @Column(name = "disease")
+  private String disease;
+
+  @Column(name = "medication_given")
+  private String medicationGiven;
+
+  @Column(name = "remarks")
+  private String remarks;
+
+}
