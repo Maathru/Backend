8937ef4b019c482e770b6b47cc05c34946c06317
Chamika Madhushan <chamikamadhushan35@gmail.com>
2024-11-30 12:09:10 +0530

    repos fixed

--- src/main/java/com/maathru/backend/External/repository/ParentRepository.java
+++ src/main/java/com/maathru/backend/External/repository/ParentRepository.java
@@ -1,7 +1,12 @@
 package com.maathru.backend.External.repository;
 
 import com.maathru.backend.Domain.entity.Parent;
+import com.maathru.backend.Domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ParentRepository extends JpaRepository<Parent, Long> {
+    Parent findByUser(User currentUser);
 }
--- src/main/java/com/maathru/backend/External/repository/PregnancyCardRepository.java
+++ src/main/java/com/maathru/backend/External/repository/PregnancyCardRepository.java
@@ -1,7 +1,13 @@
 package com.maathru.backend.External.repository;
 
+import com.maathru.backend.Domain.entity.Parent;
 import com.maathru.backend.Domain.entity.PregnancyCard;
+import com.maathru.backend.Domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PregnancyCardRepository extends JpaRepository<PregnancyCard, Long> {
+    List<PregnancyCard> findByParent(Parent parent);
 }
