85b972a933aef7aeab541f9ed9169c9ade2950e0
Buddhika Senanayake <bnsbuddhika@gmail.com>
2024-12-03 01:42:05 +0530

    Edit profile done

--- src/main/java/com/maathru/backend/Application/dto/response/UserProfileDto.java
+++ src/main/java/com/maathru/backend/Application/dto/response/UserProfileDto.java
@@ -1,17 +1,32 @@
 package com.maathru.backend.Application.dto.response;
 
+import com.maathru.backend.enumeration.Gender;
 import com.maathru.backend.enumeration.Role;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 public class UserProfileDto {
     private long id;
-    private String name;
+    private String firstName;
+    private String lastName;
     private String email;
     private Role role;
     private LocalDateTime lastLogin;
+
+    private String phoneNumber;
+    private String nic;
+    private Gender gender;
+    private LocalDate dob;
+    private String addressLine1;
+    private String street;
+    private String city;
+    private String designation;
+    private String qualifications;
+    private String moh;
+    private String region;
 }
